62608cfa0b996834a0af5735e24d476082a518c7
Daniel Aguilera <daniel.aguilera@aurea.com>
2021-07-30 12:50:45 -0300

    Refactoring

---
+++ Common/GraphHelper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+
+namespace ShortestPath
+{
+    public class GraphHelper
+    {
+        private static Random random = new Random();
+
+        public static Graph CreateGraph()
+        {
+            const int sizeX = 31;
+            const int sizeY = 16;
+
+            var graph = new Graph();
+
+            var offsets = Enumerable.Empty<PointF>()
+                .Concat(new[] { -1, 0, 1 }.SelectMany(y => new[] { -1, 0, 1 }.Select(x => new PointF(x, y))))
+                .Concat(new[] { -2, 2 }.SelectMany(y => new[] { -1, 1 }.Select(x => new PointF(x, y))))
+                .Concat(new[] { -1, 1 }.SelectMany(y => new[] { -2, 2 }.Select(x => new PointF(x, y))))
+                .Where(p => p.X != 0 || p.Y != 0)
+                .ToArray();
+
+            // add all vertices
+            graph.AddRange(Enumerable.Range(0, sizeY)
+                .SelectMany(y => Enumerable.Range(0, sizeX).Select(x => new Vertex(x, y))));
+
+            // remove some to create obstacles
+            var obstacleOffsets = offsets
+                .Concat(new[] { -2, 2 }.Select(x => new PointF(x, 0)))
+                .Concat(new[] { -2, 2 }.Select(y => new PointF(0, y)))
+                .ToArray();
+
+            for (var i = 0; i < 10; i++)
+            {
+                var index = random.Next(graph.Count);
+                var vertex = graph.Skip(index).First();
+                var toRemove = obstacleOffsets
+                    .Select(offset => graph[vertex.X + offset.X, vertex.Y + offset.Y])
+                    .Where(v => v != null)
+                    .Concat(new[] { vertex });
+
+                foreach (var item in toRemove)
+                {
+                    graph.Remove(item);
+                }
+            }
+
+            // initialize neighbors
+            foreach (var vertex in graph)
+            {
+                vertex.Neighbors = offsets
+                    .Select(offset => graph[vertex.X + offset.X, vertex.Y + offset.Y])
+                    .Where(v => v != null)
+                    .ToList();
+            }
+
+            return graph;
+        }
+
+        public static Vertex GetRandomVertex(Graph graph)
+        {
+            return graph.Skip(random.Next(graph.Count)).First();
+        }
+    }
+}
--- WindowsFormsApp/Form1.cs
+++ WindowsFormsApp/Form1.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Drawing;
-using System.Linq;
 using System.Windows.Forms;
 using ShortestPath;
 
@@ -9,7 +8,6 @@ namespace WindowsFormsApp
 {
     public partial class Form1 : Form
     {
-        private static Random random = new Random();
         private static ShortestPathAlgo algo = new DijkstraWithQueueAlgo();
         private static Context context = new Context();
 
@@ -78,8 +76,8 @@ namespace WindowsFormsApp
 
         private void Randomize()
         {
-            context.Graph = CreateGraph();
-            context.Target = context.Graph.Skip(random.Next(context.Graph.Count)).First();
+            context.Graph = GraphHelper.CreateGraph();
+            context.Target = GraphHelper.GetRandomVertex(context.Graph);
         }
 
         private void InitContext()
@@ -136,57 +134,6 @@ namespace WindowsFormsApp
             var y1 = Convert.ToInt32(y / context.Zoom - 1);
 
             return context.Graph[x1, y1];
-        }
-
-        private static Graph CreateGraph()
-        {
-            const int sizeX = 31;
-            const int sizeY = 16;
-
-            var graph = new Graph();
-
-            var offsets = Enumerable.Empty<PointF>()
-                .Concat(new[] { -1, 0, 1 }.SelectMany(y => new[] { -1, 0, 1 }.Select(x => new PointF(x, y))))
-                .Concat(new[] { -2, 2 }.SelectMany(y => new[] { -1, 1 }.Select(x => new PointF(x, y))))
-                .Concat(new[] { -1, 1 }.SelectMany(y => new[] { -2, 2 }.Select(x => new PointF(x, y))))
-                .Where(p => p.X != 0 || p.Y != 0)
-                .ToArray();
-
-            // add all vertices
-            graph.AddRange(Enumerable.Range(0, sizeY)
-                .SelectMany(y => Enumerable.Range(0, sizeX).Select(x => new Vertex(x, y))));
-
-            // remove some to create obstacles
-            var obstacleOffsets = offsets
-                .Concat(new[] { -2, 2 }.Select(x => new PointF(x, 0)))
-                .Concat(new[] { -2, 2 }.Select(y => new PointF(0, y)))
-                .ToArray();
-
-            for (var i = 0; i < 10; i++)
-            {
-                var index = random.Next(graph.Count);
-                var vertex = graph.Skip(index).First();
-                var toRemove = obstacleOffsets
-                    .Select(offset => graph[vertex.X + offset.X, vertex.Y + offset.Y])
-                    .Where(v => v != null)
-                    .Concat(new[] { vertex });
-
-                foreach (var item in toRemove)
-                {
-                    graph.Remove(item);
-                }
-            }
-
-            // initialize neighbors
-            foreach (var vertex in graph)
-            {
-                vertex.Neighbors = offsets
-                    .Select(offset => graph[vertex.X + offset.X, vertex.Y + offset.Y])
-                    .Where(v => v != null)
-                    .ToList();
-            }
-
-            return graph;
         }
     }
 }
