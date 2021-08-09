eec86c569eb0ccde28257539557886db7b32c06f
Daniel Aguilera <daniel.aguilera@aurea.com>
2021-08-09 19:54:07 -0300

    Added stress test project

---
+++ Tests/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortestPath.Tests
+{
+    class Program
+    {
+        private static Graph graph;
+        private static Vertex target;
+        private static ShortestPathAlgo algo;
+
+        private static void Main(string[] args)
+        {
+            algo = new DijkstraWithQueueAlgo();
+
+            Randomize();
+
+            Test1();
+        }
+
+        private static void Test1()
+        {
+            for (var i = 0; i < 1000; i++)
+            {
+                Recalc();
+            }
+        }
+
+        private static void Randomize()
+        {
+            graph = GraphHelper.CreateGraph();
+            target = GraphHelper.GetRandomVertex(graph);
+        }
+
+        private static void Recalc()
+        {
+            algo.Process(graph, target);
+        }
+    }
+}
